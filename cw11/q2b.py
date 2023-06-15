140bddc4d67d3047c967fac564604371d1b77982
Arad <alirezaarvin95@gmail.com>
2023-06-15 16:43:22 +0330

    q2 part b and c done

---
+++ cw11/q2b.py
@@ -0,0 +1,23 @@
+import argparse
+import os
+from pathlib import Path
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-l", "--long", action="store_true", required=False)
+parser.add_argument("path")
+
+arg = parser.parse_args()
+
+target_dir = os.listdir(arg.path)
+
+if arg.long:
+    for entry in os.listdir(arg.size):
+        if os.path.isfile(entry):
+            target_size = os.path.getsize(entry)
+            print(f"{entry}, Size: {target_size} KB")
+        else:
+            print(entry)
+else:   
+    for entry in target_dir:
+        print(entry)

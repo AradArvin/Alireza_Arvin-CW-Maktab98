e3e7bad3aab288e2759f3fa664021c70342ede1f
Arad <alirezaarvin95@gmail.com>
2023-06-15 15:19:25 +0330

    poem file path changed and q1 finnished

--- cw11/q1.py
+++ cw11/q1.py
@@ -1,19 +1,20 @@
 import sys
 import os
-# from pathlib import Path
+from pathlib import Path
 
 def read_file(file_name):
     with open(file_name, "r") as file:
-        file.read()
+        return file.read()
 
 
 if len(sys.argv) == 1:
     print("Hello World!")
 elif os.path.exists(sys.argv[1]):
     string = read_file(sys.argv[1])
-    lst1 = string.split(" ").split(".")
-    # dict1 = {}
-
-
-elif len(sys.argv) == 2:
+    lst1 = string.split(" ")
+    dict1 = dict()
+    for word in lst1:
+        dict1[word] = dict1.get(word,0) + 1
+    print(dict1)
+else:
     print(sys.argv[1][::-1])

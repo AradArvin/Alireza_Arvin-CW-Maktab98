de0d0b3f3b9238d32c478f8a5fb8be69a240ce5d
Arad <alirezaarvin95@gmail.com>
2023-06-16 14:57:16 +0330

    cw11 fri q3 comments added and part 3 title done

--- cw11 fri/dvdrental_restore.sql
+++ cw11 fri/dvdrental_restore.sql
@@ -1,5 +1,17 @@
+-- part 1
 SELECT * FROM film 
 WHERE rental_duration > 4;
 
+-- part 2
 SELECT * FROM film 
-WHERE 2 < rental_duration and rental_duration < 5;
+WHERE 2 < rental_duration AND rental_duration < 5;
+
+-- part 3 ascending
+SELECT * FROM film 
+WHERE 2 < rental_duration AND rental_duration < 5
+ORDER BY title ASC;
+
+-- part 3 descending
+SELECT * FROM film 
+WHERE 2 < rental_duration AND rental_duration < 5
+ORDER BY title DESC;

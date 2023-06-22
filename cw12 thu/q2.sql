4838650700a583dd46b3c6e060a90fee27bd2654
Arad <alirezaarvin95@gmail.com>
2023-06-22 14:46:32 +0330

    cw12 thu q2 part2 done

--- cw12 thu/q2.sql
+++ cw12 thu/q2.sql
@@ -4,3 +4,11 @@ JOIN film_category AS fc
 ON f.film_id = fc.film_id
 JOIN category AS ca
 ON fc.category_id = ca.category_id;
+
+-- Part2
+SELECT f.film_id, f.title, ca.name, f.release_year FROM film AS f
+JOIN film_category AS fc
+ON f.film_id = fc.film_id
+JOIN category AS ca
+ON fc.category_id = ca.category_id
+WHERE ca.name IN ('Action','Comedy','Family');

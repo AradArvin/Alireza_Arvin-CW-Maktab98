f45569e2b9fb47332f86c419161aa24dd81ef857
Arad <alirezaarvin95@gmail.com>
2023-06-22 14:38:30 +0330

    cw12 thu q2 part1 done

---
+++ cw12 thu/q2.sql
@@ -0,0 +1,6 @@
+-- Part1
+SELECT f.film_id, f.title, ca.name, f.release_year FROM film AS f
+JOIN film_category AS fc
+ON f.film_id = fc.film_id
+JOIN category AS ca
+ON fc.category_id = ca.category_id;

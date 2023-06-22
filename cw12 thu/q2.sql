f039297276541ba4cfd4c70f2a2e8cf29d055f09
Arad <alirezaarvin95@gmail.com>
2023-06-22 14:55:25 +0330

    cw12 thu q2 part3 done

--- cw12 thu/q2.sql
+++ cw12 thu/q2.sql
@@ -12,3 +12,11 @@ ON f.film_id = fc.film_id
 JOIN category AS ca
 ON fc.category_id = ca.category_id
 WHERE ca.name IN ('Action','Comedy','Family');
+
+-- Part3
+SELECT ca.name, Count(f.title) AS film_count FROM category AS ca
+JOIN film_category AS fc
+ON ca.category_id = fc.category_id
+JOIN film AS f
+ON fc.film_id = f.film_id
+GROUP BY ca.name;

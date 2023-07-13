8a3714e5ab17069802b67e650b39809e99f5f7ed
Arad <aradarvinxo@gmail.com>
2023-07-13 16:04:19 +0330

    django-blog models done

--- BlogPlatform/BlogApp/models.py
+++ BlogPlatform/BlogApp/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey(
+        "auth.User",
+        on_delete=models.CASCADE,
+        default="auth.User",
+    )
+    publication_date = models.DateField
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Comment(models.Model):
+    post = models.CharField(max_length=100)
+    author = models.CharField(max_length=50)
+    content = models.TextField()
+    date = models.DateField()
+
+    def __str__(self) -> str:
+        return self.post, self.author
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+    bio = models.TextField()
+
+    def __str__(self) -> str:
+        return self.name, self.bio

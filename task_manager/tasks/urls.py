5a30af06463688719737619da75a83c9a4bcbebd
Arad <aradarvinxo@gmail.com>
2023-07-21 14:46:24 +0330

    feat: new urls added for category and tag

--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -8,6 +8,11 @@ from .views import (
     search_task_result,
     category,
     category_detail,
+    category_update,
+    create_tag,
+    tag_detail,
+    all_tags,
+    update_tag,
 )
 
 
@@ -20,4 +25,9 @@ urlpatterns = [
     path("search_result/", search_task_result, name="search_task_result"),
     path("category/", category, name="category"),
     path("category/<int:pk>/", category_detail, name="category_detail"),
+    path("category-update/<int:pk>/", category_update, name="category_update"),
+    path("ctag/<int:pk>/", create_tag, name="create_tag"),
+    path("tag/<int:pk>/", tag_detail, name="tag_detail"),
+    path("tag/", all_tags, name="all_tags"),
+    path("utag/<int:pk>/", update_tag, name="update_tag"),
 ]

e14482b98ab1beffae037bf66b508950340de9ca
Arad <aradarvinxo@gmail.com>
2023-07-21 14:47:03 +0330

    feat: view for tag and category update added

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -10,7 +10,7 @@ from django.core.paginator import Paginator
 
 def home(request):
     task_list = Task.objects.filter(Q(status__contains="Ongoing"))
-    pagination = Paginator(task_list, 2)
+    pagination = Paginator(task_list, 4)
     page = request.GET.get("page")
     tasks = pagination.get_page(page)
     context = {"tasks": tasks}
@@ -29,6 +29,17 @@ def all_tasks(request):
         tag_list = []
         for tag in tag_s:
             tag_list.append(Tag.objects.get(pk=tag))
+        if request.POST.get("file"):
+            file = request.POST.get("file")
+            Task.objects.create(
+                title=title,
+                description=description,
+                due_date=due_date,
+                status=status,
+                category=category,
+                file=file,
+            ).tag.set(tag_list)
+            return redirect("all_tasks")
         Task.objects.create(
             title=title,
             description=description,
@@ -55,7 +66,75 @@ def all_tasks(request):
 
 def task_detail(request, pk):
     task = Task.objects.get(pk=pk)
-    return render(request, "tasks/task_detail.html", {"task": task})
+    if request.method == "POST":
+        title = request.POST.get("title")
+        description = request.POST.get("description")
+        due_date = request.POST.get("due_date")
+        status = request.POST.get("status")
+        category_id = request.POST.get("category")
+        category = Category.objects.get(pk=category_id)
+        tag_s = request.POST.get("tag")
+        tag_list = []
+        if tag_s:
+            for tag in tag_s:
+                tag_list.append(Tag.objects.get(pk=tag))
+        if request.POST.get("file"):
+            file = request.POST.get("file")
+            task.file = file
+        if title:
+            task.title = title
+        if description:
+            task.description = description
+        if due_date:
+            task.due_date = due_date
+        if status:
+            task.status = status
+        if category:
+            task.category = category
+        if tag_list != []:
+            task.tag.set(tag_list)
+        task.save()
+        return redirect("task_detail", pk)
+
+    else:
+        task = Task.objects.get(pk=pk)
+        category = Category.objects.all()
+        tags = Tag.objects.all()
+        context = {
+            "task": task,
+            "category": category,
+            "tags": tags,
+            "status": dict(Task.STATUS_LIST),
+        }
+        return render(request, "tasks/task_detail.html", context)
+
+
+def create_tag(request, pk):
+    if request.method == "POST":
+        tag = request.POST.get("name")
+        Tag.objects.create(name=tag)
+        return redirect("task_detail", pk)
+
+
+def tag_detail(request, pk):
+    tag = Tag.objects.get(pk=pk)
+    context = {"tag": tag}
+    return render(request, "tasks/tag_detail.html", context)
+
+
+def all_tags(request):
+    tags = Tag.objects.all()
+    context = {"tags": tags}
+    return render(request, "tasks/all_tags.html", context)
+
+
+def update_tag(request, pk):
+    tag = Tag.objects.get(pk=pk)
+    if request.method == "POST":
+        name = request.POST.get("name")
+        tag.name = name
+        tag.save()
+        return redirect("tag_detail", pk)
 
 
 def search_task(request):
@@ -78,8 +157,13 @@ def category(request):
     if request.method == "POST":
         name = request.POST.get("name")
         description = request.POST.get("description")
-        Category.objects.create(name=name, description=description)
-        return redirect("category")
+        if request.POST.get("image"):
+            image = request.POST.get("image")
+            Category.objects.create(image=image, name=name, description=description)
+            return redirect("category")
+        else:
+            Category.objects.create(name=name, description=description)
+            return redirect("category")
     else:
         categories = Category.objects.all()
         context = {"categories": categories}
@@ -97,14 +181,26 @@ def category_detail(request, pk):
         tag_list = []
         for tag in tag_s:
             tag_list.append(Tag.objects.get(pk=tag))
-        Task.objects.create(
-            title=title,
-            description=description,
-            due_date=due_date,
-            status=status,
-            category=category,
-        ).tag.set(tag_list)
-        return redirect("category_detail", pk)
+        if request.POST.get("file"):
+            file = request.POST.get("file")
+            Task.objects.create(
+                title=title,
+                description=description,
+                due_date=due_date,
+                status=status,
+                category=category,
+                file=file,
+            ).tag.set(tag_list)
+            return redirect("category_detail", pk)
+        else:
+            Task.objects.create(
+                title=title,
+                description=description,
+                due_date=due_date,
+                status=status,
+                category=category,
+            ).tag.set(tag_list)
+            return redirect("category_detail", pk)
     else:
         category = Category.objects.get(pk=pk)
         tags = Tag.objects.all()
@@ -114,6 +210,22 @@ def category_detail(request, pk):
             "status": dict(Task.STATUS_LIST),
         }
         return render(request, "tasks/category_detail.html", context)
+
+
+def category_update(request, pk):
+    category = Category.objects.get(pk=pk)
+    if request.method == "POST":
+        name = request.POST.get("name2")
+        description = request.POST.get("description2")
+        if request.POST.get("image2"):
+            image = request.POST.get("image2")
+            category.image = image
+        if name:
+            category.name = name
+        if description:
+            category.description = description
+        category.save()
+        return redirect("category_detail", pk)
 
 
 def author(request):

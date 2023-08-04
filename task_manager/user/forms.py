336a76275ec7e901c51e778488832140527c81c8
Arad <aradarvinxo@gmail.com>
2023-08-04 09:18:18 +0330

    refactor: confirm pass & clean-data method

--- task_manager/user/forms.py
+++ task_manager/user/forms.py
@@ -5,6 +5,16 @@ from .models import CustomUser
 
 class UserForm(ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
 
     class Meta:
         model = CustomUser
+        fields = ["username", "email", "password"]
+
+    def clean_data(self):
+        cleaned_data = super(CustomUser, self).clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password != confirm_password:
+            raise forms.ValidationError("Password confirmation failed")

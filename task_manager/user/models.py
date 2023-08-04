71b19fff49612cf809cc8e2dac3e8929c72adde7
Arad <aradarvinxo@gmail.com>
2023-08-04 09:18:47 +0330

    fix: unique user field

--- task_manager/user/models.py
+++ task_manager/user/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 # Create your models here.
 class CustomUser(AbstractUser):
-    username = models.CharField(max_length=20)
+    username = models.CharField(max_length=20, unique=True)
     email = models.EmailField()
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)

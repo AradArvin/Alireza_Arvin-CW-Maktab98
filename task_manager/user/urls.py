cff6f041bf63b9283056505aff44c7e14c279709
Arad <aradarvinxo@gmail.com>
2023-08-04 15:51:12 +0330

    refactor: urls for register and login done

--- task_manager/user/urls.py
+++ task_manager/user/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import 
+
+from .views import register, login
 
 urlpatterns = [
-    path(""),
+    path("register/", register, name="register"),
+    path("login/", login, name="login"),
 ]

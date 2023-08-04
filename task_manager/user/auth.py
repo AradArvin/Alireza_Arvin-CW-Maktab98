a79514fc0006fc7458283e39185748f663a88403
Arad <aradarvinxo@gmail.com>
2023-08-04 17:51:40 +0330

    refactor: auth updated

--- task_manager/user/auth.py
+++ task_manager/user/auth.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth import get_user_model
+from django.db.models import Q
 from .models import CustomUser
 
 
@@ -7,12 +8,11 @@ class CustomeEmailBack(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         UserModel = get_user_model()
         try:
-            user = UserModel.objects.get(email=username) or UserModel.objects.get(
-                username=username
+            user = UserModel.objects.get(
+                Q(username__iexact=username) | Q(email__iexact=username)
             )
         except UserModel.DoesNotExist:
             return None
         else:
             if user.check_password(password):
                 return user
-        return None

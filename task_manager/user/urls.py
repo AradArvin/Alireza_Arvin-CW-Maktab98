bdda217f237e77a571c43535fd8143ec1de59c76
Arad <aradarvinxo@gmail.com>
2023-08-04 17:53:13 +0330

    refactor: login view name changed

--- task_manager/user/urls.py
+++ task_manager/user/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import register, login
+from .views import register, logging
 
 urlpatterns = [
     path("register/", register, name="register"),
-    path("login/", login, name="login"),
+    path("login/", logging, name="login"),
 ]
--- task_manager/user/views.py
+++ task_manager/user/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 from .forms import CreateUserForm, LoginUserForm
-from .models import CustomUser
+from .auth import CustomeEmailBack
 
 # Create your views here.
 
@@ -11,9 +12,33 @@ def register(request):
         if not form.is_valid():
             return redirect("register")
         if form.is_valid():
+            user = form.save(commit=False)
+            password = user.password
+            user.set_password(password)
             form.save()
             return redirect("home")
     else:
-        form = CreateUserForm(request.POST)
+        form = CreateUserForm()
         context = {"form": form}
         return render(request, "user/register.html", context)
+
+
+def logging(request):
+    if request.method == "POST":
+        form = LoginUserForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            auth = authenticate(request=request, username=username, password=password)
+            print(auth)
+            if auth is not None:
+                login(request, auth)
+                return redirect("home")
+            else:
+                return redirect("login")
+        else:
+            return redirect("login")
+    else:
+        form = LoginUserForm()
+        context = {"form": form}
+        return render(request, "user/login.html", context)

f5319b5c23925b4fb8e7fb63f87df970dab8452d
Arad <aradarvinxo@gmail.com>
2023-08-04 15:54:04 +0330

    refactor: view for register done

--- task_manager/user/views.py
+++ task_manager/user/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CreateUserForm, LoginUserForm
+from .models import CustomUser
 
 # Create your views here.
+
+
+def register(request):
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if not form.is_valid():
+            return redirect("register")
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+    else:
+        form = CreateUserForm(request.POST)
+        context = {"form": form}
+        return render(request, "user/register.html", context)

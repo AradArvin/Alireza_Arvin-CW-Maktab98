72da373f9a76fd934d9d22b901c6c9a4de94dd0b
Arad <aradarvinxo@gmail.com>
2023-08-11 10:05:32 +0330

    refactoe: clean code

--- task_manager/tasks/mixins.py
+++ task_manager/tasks/mixins.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render, redirect
 from django.core.exceptions import PermissionDenied
 from .models import Task
-from django.views import View
 
 
-class TodoOwnerRequiredMixin(View):
-    model = Task
-    template_name = None
+
+class TodoOwnerRequiredMixin:
 
     def dispatch(self, request, *args, **kwargs):
-        todo = Task.objects.get(id=kwargs['pk'])
-        if not todo.user == request.user:
+        task = Task.objects.get(id=kwargs['pk'])
+        if not task.user == request.user:
             raise PermissionDenied
         else:
             return super().dispatch(request, *args, **kwargs)

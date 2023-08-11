36cad9deb34a09f87d61c067d4f23ce079683c59
Arad <aradarvinxo@gmail.com>
2023-08-11 11:17:40 +0330

    fix: request.user

--- task_manager/user/mixins.py
+++ task_manager/user/mixins.py
@@ -6,8 +6,8 @@ from .models import CustomUser
 class ProfileMixin:
 
     def dispatch(self, request, *args, **kwargs):
-        user = CustomUser.objects.get(id=kwargs['pk'])
-        if not user.user == request.user:
+        user = request.user
+        if not user == request.user:
             raise PermissionDenied
         else:
             return super().dispatch(request, *args, **kwargs)

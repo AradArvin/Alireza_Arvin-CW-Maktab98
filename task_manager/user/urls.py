312e51fdb0c71b17aeef44160c505b5d22900078
Arad <aradarvinxo@gmail.com>
2023-08-11 11:17:58 +0330

    feat: new link for profile add

--- task_manager/user/urls.py
+++ task_manager/user/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import register, logging
+from .views import register, logging, ProfileView
 
 urlpatterns = [
     path("register/", register, name="register"),
     path("login/", logging, name="login"),
+    path("profile/", ProfileView.as_view(), name="profile"),
 ]

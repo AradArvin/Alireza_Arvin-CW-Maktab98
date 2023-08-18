f5e6670461789ee531e0bfbca2bb62bbd0549f9f
Arad <aradarvinxo@gmail.com>
2023-08-18 10:57:05 +0330

    refactor: filter added to number of tasks

--- task_manager/user/admin.py
+++ task_manager/user/admin.py
@@ -4,16 +4,24 @@ from django.db.models.query import QuerySet
 from django.http.request import HttpRequest
 from .models import CustomUser
 from tasks.models import Task
+from django.db.models import Count
 # Register your models here.
 
 
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ("username", "email","number_of_tasks")
-    list_per_page = 10
-    
+
+    def get_queryset(self, request: HttpRequest) -> QuerySet[Any]:
+        queryst = super().get_queryset(request)
+        queryst = queryst.annotate(number_of_tasks=Count("task"))
+        return queryst
+
     def number_of_tasks(self, obj):
-        num = Task.objects.filter(user=obj).count()
+        num = obj.number_of_tasks
         return num
     
-# num = Task.objects.filter(title=obj).count()
+    list_display = ("username", "email","number_of_tasks")
+    number_of_tasks.admin_order_field = "number_of_tasks"
+    list_per_page = 10
+    
+# num = Task.objects.filter(user=obj).count()

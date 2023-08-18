578e7292ec8caec870771c02b9f6e0df9d26a0a5
Arad <aradarvinxo@gmail.com>
2023-08-18 11:44:42 +0330

    feat: great filter is done

--- task_manager/user/admin.py
+++ task_manager/user/admin.py
@@ -8,6 +8,23 @@ from django.db.models import Count
 # Register your models here.
 
 
+class GreatFilter(admin.SimpleListFilter):
+    title = "is_great"
+    parameter_name = "great"
+
+    def lookups(self, request, modeladmin):
+        return [
+            ("great", "is great"),
+            ("not_great", "not great"),
+        ]
+    
+    def queryset(self, request, queryset):
+        if self.value() == "great":
+            return queryset.filter(number_of_tasks__gt=10).distinct()
+        
+        if self.value() == "not_great":
+            return queryset.filter(number_of_tasks__lt=10).distinct()
+
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
 
@@ -22,6 +39,8 @@ class CustomUserAdmin(admin.ModelAdmin):
     
     list_display = ("username", "email","number_of_tasks")
     number_of_tasks.admin_order_field = "number_of_tasks"
+    list_filter = (GreatFilter,)
     list_per_page = 10
     
 # num = Task.objects.filter(user=obj).count()
+

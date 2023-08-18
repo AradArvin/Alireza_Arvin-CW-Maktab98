8c34bcfe6eb70c74bc91e728ac8fc583600393d5
Arad <aradarvinxo@gmail.com>
2023-08-18 10:33:19 +0330

    feat: number of tasks for admin user

--- task_manager/user/admin.py
+++ task_manager/user/admin.py
@@ -1,5 +1,19 @@
+from typing import Any
 from django.contrib import admin
+from django.db.models.query import QuerySet
+from django.http.request import HttpRequest
 from .models import CustomUser
-
+from tasks.models import Task
 # Register your models here.
-admin.site.register(CustomUser)
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ("username", "email","number_of_tasks")
+    list_per_page = 10
+    
+    def number_of_tasks(self, obj):
+        num = Task.objects.filter(user=obj).count()
+        return num
+    
+# num = Task.objects.filter(title=obj).count()
